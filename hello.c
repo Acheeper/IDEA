966ea65170e0c2c06bcfba94d9ae06a22c2df248
yong <kabudac@outlook.com>
2026-01-06 14:23:09 +0800

    向hello.c添加两个函数

--- hello.c
+++ hello.c
@@ -1,5 +1,14 @@
 #include "stdio.h"
 
+void say_hello() {
+    printf("Hello from say_hello function!\n");
+}
+
+void say_byebye()
+{
+    printf("bye");
+}
+
 int main() {
     printf("Hello, World!\n");
     return 0;
